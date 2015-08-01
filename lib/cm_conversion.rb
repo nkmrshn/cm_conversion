b5f8dd8937ac60ceeb002bc6a2d6eb9ae0a939f7
vagrant <e1501ta@aiit.ac.jp>
2015-08-01 03:26:56 +0000

    add to_shaku method

--- lib/cm_conversion.rb
+++ lib/cm_conversion.rb
@@ -5,5 +5,9 @@ module CmConversion
     def to_feet(value)
       value.is_a?(Numeric) ? value / 30.48 : nil
     end
+
+    def to_shaku(value)
+      value.is_a?(Numeric) ? value / 30.303 : nil
+    end
   end
 end
